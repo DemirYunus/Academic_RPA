ae95d187ac7d495e21b92c4237d1a0e02ba3b353
Yunus DEMİR <110460055+DemirYunus@users.noreply.github.com>
2026-08-07 22:54:09 +0300

    Haydi bismillah

---
+++ RPA/RPA.GRASP/TimeHorizon.cs
@@ -0,0 +1,85 @@
+﻿namespace RPA.GRASP
+{
+    public class TimeHorizon
+    {
+        public int Start { get; set; }
+        public int End { get; set; }
+
+        public TimeHorizon(int start, int end)
+        {
+            Start = start;
+            End = end;
+        }
+
+        public override string ToString()
+        {
+            return $"{Start}-{End}";
+        }
+    }
+
+    public class ResourceScheduler
+    {
+        private const int HorizonStart = 0;
+        private const int HorizonEnd = 1440;
+
+        // Kaynakların boş zamanlarını (idle times) tutan koleksiyon
+        private Dictionary<string, List<TimeHorizon>> _idleTimes;
+
+        public ResourceScheduler(IEnumerable<string> resources)
+        {
+            _idleTimes = new Dictionary<string, List<TimeHorizon>>();
+
+            foreach (var res in resources)
+            {
+                // Başlangıçta tüm ufuk (0-1440) boş kabul edilir
+                _idleTimes[res] = new List<TimeHorizon> { new TimeHorizon(HorizonStart, HorizonEnd) };
+            }
+        }
+
+        // Yeni operasyon ekleyen ve boş zamanları güncelleyen metot
+        public void AddOperation(string resourceId, int opStart, int opEnd)
+        {
+            if (!_idleTimes.ContainsKey(resourceId))
+            {
+                throw new ArgumentException("Geçersiz kaynak kimliği.");
+            }
+
+            var currentIdleTimes = _idleTimes[resourceId];
+            var newIdleTimes = new List<TimeHorizon>();
+
+            foreach (var idle in currentIdleTimes)
+            {
+                // Eğer operasyon mevcut boşlukla kesişmiyorsa, boşluğu aynen koru
+                if (opEnd <= idle.Start || opStart >= idle.End)
+                {
+                    newIdleTimes.Add(idle);
+                }
+                else
+                {
+                    // Kesişme durumu: Boşluğu parçala (öncesi ve sonrası olarak)
+                    if (idle.Start < opStart)
+                    {
+                        newIdleTimes.Add(new TimeHorizon(idle.Start, opStart));
+                    }
+                    if (idle.End > opEnd)
+                    {
+                        newIdleTimes.Add(new TimeHorizon(opEnd, idle.End));
+                    }
+                }
+            }
+
+            // Güncellenmiş boşlukları zamana göre sıralayarak kaydet
+            _idleTimes[resourceId] = newIdleTimes.OrderBy(x => x.Start).ToList();
+        }
+
+        // İstendiğinde bir kaynağın boş zamanlarına erişmek için kullanılan metot
+        public List<TimeHorizon> GetIdleTimes(string resourceId)
+        {
+            if (_idleTimes.ContainsKey(resourceId))
+            {
+                return _idleTimes[resourceId];
+            }
+            return new List<TimeHorizon>();
+        }
+    }
+}
--- RPA/RPA.PL/Program.cs
+++ RPA/RPA.PL/Program.cs
@@ -5,6 +5,8 @@ using System.ComponentModel;
 using System.Data;
 using System.Diagnostics;
 using RPA.DAL;
+using RPA.GRASP;
+
 
 
 
@@ -12,25 +14,68 @@ using RPA.DAL;
 //DataTable dtProcess = pg.GenerateProcessTable(4, 4);
 //DataTable dtProcessInstances = pg.GenerateProcessInstanceTable(dtProcess);
 
-GetData gd = new GetData();
-DataTable dtProcess = gd.GetProcessData();
-DataTable dtProcessInstances = gd.GetProcessInstanceData();
+//GetData gd = new GetData();
+//DataTable dtProcess = gd.GetProcessData();
+//DataTable dtProcessInstances = gd.GetProcessInstanceData();
 
-double[] costOfSoftare = new double[3];
-costOfSoftare[0] = 100;
-costOfSoftare[1] = 150;
-costOfSoftare[2] = 200;
+//double[] costOfSoftare = new double[3];
+//costOfSoftare[0] = 100;
+//costOfSoftare[1] = 150;
+//costOfSoftare[2] = 200;
 
-ILP model = new ILP(dtProcess, dtProcessInstances);
-model.Solve(4, 4, 3, 7, 3000, costOfSoftare, 1, 1440); //10080
-int[,] yValue = model.PrintYValue();
-int[,] xValue = model.PrintXValue();
-int[] hValue = model.PrintHValue();
-double[,] sValue= model.PrintSValue();
-double[,] ssValue = model.PrintSSValue();
-int[,,] zValue = model.PrintZValue();
+//ILP model = new ILP(dtProcess, dtProcessInstances);
+//model.Solve(4, 4, 3, 7, 3000, costOfSoftare, 1, 1440); //10080
+//int[,] yValue = model.PrintYValue();
+//int[,] xValue = model.PrintXValue();
+//int[] hValue = model.PrintHValue();
+//double[,] sValue= model.PrintSValue();
+//double[,] ssValue = model.PrintSSValue();
+//int[,,] zValue = model.PrintZValue();
 
-DataTable dtResult = model.resultTable(dtProcessInstances);
+//DataTable dtResult = model.resultTable(dtProcessInstances);
+
+// Kaynakları tanımla ve planlayıcıyı başlat
+var resources = new List<string> { "R1", "R2", "R3" };
+var scheduler = new ResourceScheduler(resources);
+
+// R1 için ilk operasyonu ekle (360 - 420)
+scheduler.AddOperation("R1", 360, 420);
+
+// R1 için ikinci operasyonu ekle (690 - 810)
+scheduler.AddOperation("R1", 690, 810);
+
+
+// R1 için üçüncü operasyonu ekle (690 - 810)
+scheduler.AddOperation("R1", 930, 1080);
+
+// R1 için dördüncü operasyonu ekle (690 - 810)
+scheduler.AddOperation("R1", 30, 120);
+
+// Sonuçları al
+var r1IdleTimes = scheduler.GetIdleTimes("R1");
+
+// Çıktı Kontrolü
+Console.WriteLine("R1 Boş Zamanları:");
+foreach (var slot in r1IdleTimes)
+{
+    Console.WriteLine(slot.ToString());
+}
+Console.ReadLine();
+
+// Zaman penceresi sınırları
+int windowStart = 800;
+int windowEnd = 1440;
+string selectedResource = "R1";
+
+// Metodu çağır
+var results = WindowFilter.GetIdleTimesInWindow(selectedResource, r1IdleTimes, windowStart, windowEnd);
+
+// Çıktıyı yazdır
+Console.WriteLine($"{selectedResource} kaynağı için {windowStart}-{windowEnd} penceresindeki boşluklar:");
+foreach (var result in results)
+{
+    Console.WriteLine(result.ToString());
+}
 
 Console.ReadLine();
 
